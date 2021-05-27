7824016174ca64b8b989a276df7a31b6c9ae4e1d
Adison25 <emerickadison@gmail.com>
2021-05-27 13:02:34 -0700

    test for sub

--- test.py
+++ test.py
@@ -7,6 +7,9 @@ class TestCase(unittest.TestCase):
     def test_add_1(self):
         self.assertEqual(code.add(1, 2), 3)
 
+    def test_subtract_1(self):
+        self.assertEqual(code.subtract(3, 2), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
